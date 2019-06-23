51720f716751a031ace9f7a72303a7f9ecedec4d
cjd <494716102@qq.com>
2019-06-23 23:53:45 +0800

    test1

--- src/main/java/com/cjdjyf/outBuyservice/OutBuyServiceApplication.java
+++ src/main/java/com/cjdjyf/outBuyservice/OutBuyServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OutBuyServiceApplication {
 	public static void main(String[] args) {
-		System.out.println(234);
 		SpringApplication.run(OutBuyServiceApplication.class, args);
 	}
 }
